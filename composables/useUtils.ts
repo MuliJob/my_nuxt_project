b4150ba9d74ad5271e7e18cf28c75eabb51aec02
Muli Job <munyoki912@gmail.com>
2025-01-19 19:49:51 +0300

    composables

---
+++ composables/useUtils.ts
@@ -0,0 +1,7 @@
+export const useUtils = () => {
+  const sayHello = () => console.log('Hello to the use utils');
+  return {
+    sayHello,
+  };
+}
+// vueUse
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
-
+<script setup>
+const { sayHello } = useUtils();
+sayHello();
+</script>
 
 <template>
   <div>
     <!-- <Alert />
     <Profile /> -->
     Main Page
-    <IconBell />
-    <img src="/image.png" alt="" />
+    <!-- <IconBell /> -->
+    <!-- <img src="/image.png" alt="" /> -->
   </div>
 </template>
